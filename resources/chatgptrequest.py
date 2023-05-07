0866297e7da693765ed4811770260880861e169e
Nestor Borre <nestor.borre66@gmail.com>
2023-05-07 11:37:16 -0400

    Print the exception while getting response from ChatGpt

--- resources/chatgptrequest.py
+++ resources/chatgptrequest.py
@@ -75,8 +75,8 @@ class ChatGptRequest(MethodView):
         try:
             response_text = get_completion(prompt)
             response = ChatGptResponseModel(request_id=request.id, response=response_text)
-        except:
-            abort(500, message="An error occurred while getting Response.")
+        except Exception as e: 
+            print(e)
 
         try:
             db.session.add(response)
